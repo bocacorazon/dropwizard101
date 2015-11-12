fb100ee98d7bd74976f8b5c1b6a1fab20a1b7963
Marcos Ferreyra <marcos@mentolada>
2015-11-12 14:55:57 -0600

    Initial commit

---
+++ src/main/java/com/tr/mf/origin_dw/App.java
@@ -0,0 +1,13 @@
+package com.tr.mf.origin_dw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
